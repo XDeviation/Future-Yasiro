2e36590ff051e8b5aa9e779eae73e07daf1724fc
XDeviation <191602098@qq.com>
2022-11-02 01:07:12 +0800

    📃 docs: add env install

--- docs/README.md
+++ docs/README.md
@@ -4,4 +4,35 @@ Future Yasiro development documentation
 
 ## Environment
 
-- [Go](https://go.dev/) version 1.19.2
+At first, you should run ```src/scripts/install_env.sh``` to install all package
+### PostgreSQL
+
+Future Yasiro use PostgreSQL to manage all data, so you should install PostgreSQL first.
+
+In this document, we use PostgreSQL **version 15.0**, maybe lower versions will work.
+
+All steps marked with **(recommand)** can be skipped if you know what you are doing.
+
+All steps are recommended for those using psql for the first time
+
+
+1. **(recommand)** create a new role
+    ```bash
+    sudo -i -u postgres
+    psql
+    ```
+    then you will into psql cli, create db and user
+    ```psql
+    CREATE USER <user_name> WITH PASSWORD '<passwd>';
+    CREATE DATABASE <database_name> OWNER <user_name>;
+    GRANT ALL PRIVILEGES ON DATABASE <database_name> TO <user_name>;
+    \q
+    ```
+    
+2. make psql connectable
+
+    try:
+    ```bash
+    psql -U <user_name> -d <database_name> -h <database_host>
+    ```
+    If you can't connect to psql, see [this](https://stackoverflow.com/questions/69676009/psql-error-connection-to-server-on-socket-var-run-postgresql-s-pgsql-5432) answer, remember we use version 15 but not 14, so you should choice correct path
--- src/scripts/install_env.sh
+++ src/scripts/install_env.sh
@@ -1,5 +1,56 @@
 set -ex
+
+# install go-http
 wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0-rc3/go-cqhttp_linux_amd64.tar.gz -O bin/go-cqhttp.tar.gz
 mkdir ./bin/go-cqhttp/
 tar -xzvf ./bin/go-cqhttp.tar.gz -C ./bin/go-cqhttp/
 rm ./bin/go-cqhttp.tar.gz
+
+# install rabbitmq
+sudo apt-get install curl gnupg apt-transport-https -y
+
+## Team RabbitMQ's main signing key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg >/dev/null
+## Launchpad PPA that provides modern Erlang releases
+curl -1sLf "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf77f1eda57ebb1cc" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg >/dev/null
+## PackageCloud RabbitMQ repository
+curl -1sLf "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.packagecloud.rabbitmq.gpg >/dev/null
+
+## Add apt repositories maintained by Team RabbitMQ
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+## Provides modern Erlang/OTP releases
+##
+## "bionic" as distribution name should work for any reasonably recent Ubuntu or Debian release.
+## See the release to distribution mapping table in RabbitMQ doc guides to learn more.
+deb [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu bionic main
+
+## Provides RabbitMQ
+##
+## "bionic" as distribution name should work for any reasonably recent Ubuntu or Debian release.
+## See the release to distribution mapping table in RabbitMQ doc guides to learn more.
+deb [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ bionic main
+deb-src [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ bionic main
+EOF
+
+## Update package indices
+sudo apt-get update -y
+
+## Install Erlang packages
+sudo apt-get install -y erlang-base \
+    erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+    erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+    erlang-runtime-tools erlang-snmp erlang-ssl \
+    erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+## Install rabbitmq-server and its dependencies
+sudo apt-get install rabbitmq-server -y --fix-missing
+
+sudo service rabbitmq-server start
+
+# install postgresql
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql postgresql-client
