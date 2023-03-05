cb383ee69995fbd579b3275caca16d32d5fe8d72
zawedx <1071022348@qq.com>
2023-03-05 20:06:18 +0800

    add .gptg mode

--- src/plugins/chatgpt_qq/chatgpt_qq.py
+++ src/plugins/chatgpt_qq/chatgpt_qq.py
@@ -96,10 +96,28 @@ def receive_message(user_info, message):
         conversation = add_new_user(user_info, message)
     # 维护上下文，根据收到的信息进行回复
     pop_append(conversation, {"role": "user", "content": message})
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation["context"]
-    )
+    completion = None
+    try_times = 3
+    while completion == None:
+        if try_times == 0:
+            break
+        try_times -= 1
+        try:
+            # 调用外部包的代码
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=conversation["context"]
+            )
+        except Exception as e:
+            # 处理错误的代码，例如记录日志、发送通知等
+            logger.error({
+                "error_description": "openai server error",
+                "error_info":{
+                    "res_try_times": try_times,
+                    "exception_message": e
+                }
+            })
+            completion = None
     pop_append(conversation, completion.choices[0].message)
     send_message(user_info, completion.choices[0].message)
     # 更新聊天记录文件
@@ -135,9 +153,10 @@ def main():
         #     mes = message.get()
         #     if 1:
         #         return
-        message_text = message.get("message")
+        # 信息格式为".gpt message"，应当去掉前四个字符
+        message_text = message.get("message")[4:]
         user_info = {
-            "group": message.get("group"),
+            "group": message_type == 'group',
             "message_type": message_type,
             "sender": message.get("sender")
         }
@@ -205,6 +224,94 @@ def main():
     mq_channel.start_consuming()
 
     mq_connection.close()
+    
+    mq_connection_group = get_mq_connection()
+    mq_channel_group = mq_connection_group.channel()
+    mq_channel_group.queue_declare(queue=".gptg")
+
+    def callback_group(ch, method, properties, body):
+        message = json.loads(body)
+        message_type = message.get("message_type", "")
+        if message_type != 'group':
+            return
+        
+        fake_group_sender = {
+            'age': 0, 
+            'nickname': '群成员', 
+            'sex': 'unknown', 
+            'user_id': 123456789
+        }
+        # 信息格式为".gptg message"，应当去掉前五个字符
+        message_text = message.get("message")[5:]
+        user_info = {
+            "group": message_type == 'group',
+            "message_type": message_type,
+            "sender": fake_group_sender
+        }
+        if user_info["group"] == True:
+            user_info["group_id"] = message.get("group_id")
+            
+        logger.info({
+            "action": "receive",
+            "data": {
+                "user_info": user_info,
+                "message_text": message_text
+            }
+        })
+
+        receive_message(user_info, message_text)
+            
+        logger.info({
+            "action": "send",
+            "data": {
+                "user_info": user_info,
+                "message_text": message_cache
+            }
+        })
+
+        send_mq_connection = get_mq_connection()
+        send_mq_channal = send_mq_connection.channel()
+        send_mq_channal.queue_declare(queue="send_message")
+        return_msg = {
+            "message_type": "qq_message",
+            "message": None,
+        }
+        if message_type == 'private':
+            user_id = message.get("user_id", 0)
+            return_msg["message"] = { 
+                "action": "send_private_msg",
+                "params": {
+                    "user_id": user_id, 
+                    "message": message_cache,
+                    "auto_escape": True
+                }
+            }
+            
+
+        if message_type == 'group':
+            group_id = message.get("group_id", 0)
+            return_msg["message"] = { 
+                "action": "send_group_msg",
+                "params": {
+                    "group_id": group_id, 
+                    "message": message_cache,
+                    "auto_escape": True
+                }
+            }
+
+        send_mq_channal.basic_publish(
+            exchange='',
+            routing_key="send_message",
+            body=json.dumps(return_msg)
+        )
+        logger.info(f"Send message {return_msg}")
+
+    mq_channel_group.basic_consume(
+        queue=".gptg", on_message_callback=callback_group, auto_ack=True
+    )
+    mq_channel_group.start_consuming()
+
+    mq_connection_group.close()
 
 # 下面是一段测试receive_message的代码
 # test_user_info = {
