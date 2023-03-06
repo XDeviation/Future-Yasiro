65270aface10265c9547011d7d94fed12a8e94ec
XDeviation <191602098@qq.com>
2023-03-06 12:42:53 +0800

    Fix chatgpt

--- src/plugins/chatgpt_qq/__main__.py
+++ src/plugins/chatgpt_qq/__main__.py
@@ -1,7 +1,7 @@
 """
 Each plugin should have __main__.py, which Future Yasiro will start plugin from
 """
-from .chatgpt_qq import main
+from plugins.chatgpt_qq.chatgpt_qq import main
 
 if __name__ == '__main__':
     main()
