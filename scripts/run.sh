8e74f7822c0424be0c3b7f127b41cd42535e2557
XDeviation <191602098@qq.com>
2023-03-24 21:15:52 +0800

    fix scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,25 +14,14 @@ cd ../..
 
 echo "----------Init python env----------"
 
-set +e
 cd src
-poetry install
-set -e
-poetry shell
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 echo "----------Start Rec module----------"
-
 nohup python3 rec_module >/dev/null 2>&1 &
 
 echo "----------Start Send module----------"
-set +e
-unset https_proxy && unset http_proxy
-set -e
 nohup python3 send_module >/dev/null 2>&1 &
-set +e
-export https_proxy="http://${hostip}:10086" && export http_proxy="http://${hostip}:10086"
-set -e
 
 for package in $PWD/plugins/*; do
     echo "----------Running $package----------"
