6e08ed19b507296e13646160ee81249f9de82c44
XDeviation <191602098@qq.com>
2023-04-09 14:53:38 +0800

    fix scripts

--- scripts/install_env.sh
+++ scripts/install_env.sh
@@ -3,7 +3,7 @@ set -ex
 # install go-cqhttp
 inctallGoCqhttp() {
     mkdir bin
-    wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0-rc4/go-cqhttp_linux_amd64.tar.gz -O bin/go-cqhttp.tar.gz
+    wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0/go-cqhttp_linux_amd64.tar.gz -O bin/go-cqhttp.tar.gz
     mkdir ./bin/go-cqhttp/
     tar -xzvf ./bin/go-cqhttp.tar.gz -C ./bin/go-cqhttp/
     rm ./bin/go-cqhttp.tar.gz
